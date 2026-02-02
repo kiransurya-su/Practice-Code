b78047ebc036120dc3ad0c50ee89a039125a853e
kiransurya-su <inbox.kiran123@gmail.com>
2026-02-02 11:00:49 +0530

    Practicing 2D Binary Search

---
+++ src/dsa/binarysearch/two_dimensional_array/RowColMatrix.java
@@ -0,0 +1,30 @@
+package dsa.binarysearch.two_dimensional_array;
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,48,50}
+        };
+        System.out.println(Arrays.toString(search(arr,50)));
+    }
+    static int[] search(int[][] arr,int target){
+        int row=0;
+        int col=arr.length-1;
+        while(row<arr.length && col>=0){
+            if(arr[row][col]==target){
+                return new int[]{row,col};
+            }
+            if(arr[row][col]<target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
