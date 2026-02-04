0b6988cec3dcfa4d39cfd496a8f73e2c155d6e79
kiransurya-su <inbox.kiran123@gmail.com>
2026-02-04 08:04:52 +0530

    Leetcode DCC-> Feb 04 2026

---
+++ src/leetcode_dcc/TrionicArray_II.java
@@ -0,0 +1,81 @@
+package leetcode_dcc;
+/*
+3640. Trionic Array II (Hard)
+
+You are given an integer array nums of length n.
+
+A trionic subarray is a contiguous subarray nums[l...r] (with 0 <= l < r < n) for which there exist indices l < p < q < r such that:
+
+nums[l...p] is strictly increasing,
+nums[p...q] is strictly decreasing,
+nums[q...r] is strictly increasing.
+Return the maximum sum of any trionic subarray in nums.
+
+Streak-> 181
+
+ */
+public class TrionicArray_II {
+    public static void main(String[] args) {
+        int[] nums = {0,-2,-1,-3,0,2,-1};
+        System.out.println(maxSumTrionic(nums));
+    }
+    public static long maxSumTrionic(int[] nums) {
+        int n = nums.length;
+        long ans = Long.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            long res = 0;
+
+            // first segment: increasing segment
+            while (j < n && nums[j - 1] < nums[j]) {
+                j++;
+            }
+            int p = j - 1;
+
+            if (p == i) {
+                continue;
+            }
+
+            // second segment: decreasing segment
+            res += nums[p] + nums[p - 1];
+            while (j < n && nums[j - 1] > nums[j]) {
+                res += nums[j];
+                j++;
+            }
+            int q = j - 1;
+
+            if (q == p || q == n - 1 || (j < n && nums[j] <= nums[q])) {
+                i = q;
+                continue;
+            }
+
+            // third segment: increasing segment
+            res += nums[q + 1];
+
+            // find the maximum sum of the third segment
+            long maxSum = 0;
+            long sum = 0;
+            for (int k = q + 2; k < n && nums[k] > nums[k - 1]; k++) {
+                sum += nums[k];
+                maxSum = Math.max(maxSum, sum);
+            }
+            res += maxSum;
+
+            // find the maximum sum of the first segment
+            maxSum = 0;
+            sum = 0;
+            for (int k = p - 2; k >= i; k--) {
+                sum += nums[k];
+                maxSum = Math.max(maxSum, sum);
+            }
+            res += maxSum;
+
+            // update answer
+            ans = Math.max(ans, res);
+            i = q - 1;
+        }
+
+        return ans;
+    }
+}
