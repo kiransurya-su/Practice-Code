e4467e72d100f0c76a29152fff40c73a76978d7c
Kiran Surya <inbox.kiran123@gmail.com>
2026-02-25 08:34:36 +0530

    Leetcode DCC -> Feb 25 2026

---
+++ src/leetcode_dcc/feb_26/SortIntegersbyTheNumberofOneBits.java
@@ -0,0 +1,50 @@
+package leetcode_dcc.feb_26;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+1356. Sort Integers by The Number of 1 Bits (Easy)
+You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+Return the array after sorting it.
+
+
+
+Example 1:
+
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+Example 2:
+
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explantion: All integers have 1 bit in the binary representation, you should just sort them in ascending order.
+* */
+public class SortIntegersbyTheNumberofOneBits {
+    static class CustomComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer a, Integer b) {
+            if (Integer.bitCount(a) == Integer.bitCount(b)) {
+                return a - b;
+            }
+            return Integer.bitCount(a) - Integer.bitCount(b);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(sortByBits(arr)));
+    }
+
+    public static int[] sortByBits(int[] arr) {
+        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = new CustomComparator();
+        Arrays.sort(nums, comparator);
+        return Arrays.stream(nums).mapToInt(Integer::intValue).toArray();
+    }
+}
