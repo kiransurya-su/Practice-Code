9be33da9a56ff218aa3cc1dcb98b06df736de039
Kiran Surya <inbox.kiran123@gmail.com>
2026-03-30 12:01:05 +0530

    LeetCode DCC -> March 28 2026

--- src/leetcode_dcc/mar_26/FindTheStringWithLCP.java
+++ src/leetcode_dcc/mar_26/FindTheStringWithLCP.java
@@ -6,6 +6,10 @@ lcp[i][j] is equal to the length of the longest common prefix between the substr
 Given an n x n matrix lcp, return the alphabetically smallest string word that corresponds to lcp. If there is no such string, return an empty string.
 A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, string a has a letter that appears earlier in the alphabet than the corresponding letter in b. For example, "aabd" is lexicographically smaller than "aaca" because the first position they differ is at the third letter, and 'b' comes before 'c'.
 
+Input: lcp = [[4,3,2,1],[3,3,2,1],[2,2,2,1],[1,1,1,1]]
+Output: "aaaa"
+Explanation: lcp corresponds to any 4 letter string with a single distinct letter. The lexicographically smallest of them is "aaaa".
+
 Streak->234
 * */
 import java.util.*;
