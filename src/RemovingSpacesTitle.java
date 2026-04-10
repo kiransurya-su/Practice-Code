010ea1de297c577b59b4239cd8753667a17144e7
Kiran Surya <inbox.kiran123@gmail.com>
2026-04-10 09:48:25 +0530

    code for removing spaces

---
+++ src/RemovingSpacesTitle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class RemovingSpacesTitle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] st = sc.nextLine().split(" ");
+        for(String k:st){
+            System.out.print(k);
+        }
+
+    }
+}
